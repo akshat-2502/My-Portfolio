d20b6362098c8fd7d0df87f20dc7a93d221dcc5f
akshat-2502 <khulbeakshat@gmail.com>
2025-05-12 22:18:36 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="bg-[#050414]">
-      <div>//grid lines</div>
-      <div>
+    <div className="bg-[#050414] w-full">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+      <div className="w-full">
         <Navbar />
         <About />
         <Skills />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,142 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [activeSection, setActiveSection] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  //Checking scroll and changing Navbar Background
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //smooth scroll function
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+  };
+
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "projects", label: "Projects" },
+    { id: "education", label: "Education" },
+  ];
+  return (
+    <nav
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md"
+          : "bg-transparent"
+      }`}
+    >
+      <div className="text-white py-5 flex justify-between items-center">
+        <div className="text-lg md:text-2xl font-bold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white"> Akshat</span>
+          <span className="text-[#8245ec]"> / </span>
+          <span className="text-white">Khulbe </span>
+          <span className="text-[#8245ec]">&gt;</span>
+          <span className="text-[#8245ec]"></span>
+        </div>
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-8 text-gray-300 text-lg">
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={`cursor-pointer hover:text-[#8245ec] transition-all duration-500 transform hover:scale-130 ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }`}
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {" "}
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {/* Social Media Icons */}
+        <div className="hidden md:flex space-x-4">
+          <a
+            href="https://github.com/akshat-2502"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec] transition-all duration-500 transform hover:scale-130"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#0077B5] transition-all duration-500 transform hover:scale-130"
+          >
+            <FaLinkedin size={30} />
+          </a>
+        </div>
+        {/* Mobile Menu Icon */}
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
+        </div>
+      </div>
+
+      {/* Mobile Menu Items */}
+      {isOpen && (
+        <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden">
+          <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className={`cursor-pointer hover:text-white ${
+                  activeSection === item.id ? "text-[#8245ec]" : ""
+                }`}
+              >
+                <button onClick={() => handleMenuItemClick(item.id)}>
+                  {item.label}
+                </button>
+              </li>
+            ))}
+            <div className="flex space-x-4">
+              <a
+                href="https://github.com/akshat-2502"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-[#8245ec]"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-[#8245ec]"
+              >
+                <FaLinkedin />
+              </a>
+            </div>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
