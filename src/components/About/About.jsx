4cb8b05535428443fd484d92ea0cc25f2aae281a
akshat-2502 <khulbeakshat@gmail.com>
2025-08-05 13:21:51 +0530

    updated resume latest ems

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1LhelgCEf5p3Fki5Vy5L1zBtNkgR1eVd9/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1-8l9zsBnH6irzNWYCDly4a0VpMeptmHH/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
